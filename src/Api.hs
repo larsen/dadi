a5c8ac31709f73c24df0fd6336e7f6d8f620a2d9
Matteo Acerbi <matteo.acerbi@gmail.com>
2017-03-10 11:28:11 +0100

    Switch to STM

--- src/Api.hs
+++ src/Api.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS -Wall #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
@@ -14,9 +15,7 @@ import Check
 import Servant.API
 import Servant
 import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Trans.Class (lift)
-import Control.Concurrent
-import Control.Concurrent.MVar
+import Control.Concurrent.STM
 import qualified Data.Map.Strict as M
 import qualified Data.UUID as UUID
 import Network.Wai.Middleware.Cors
@@ -38,14 +37,12 @@ type DadiAPI = PostCheckRoute
           :<|> ListChecksRoute
 
 
-type DB = MVar Collection
+type DB = TVar Collection
 
 
-insertCheck :: DB -> Check -> IO Check
-insertCheck db check =
-  modifyMVar db $ \m -> do
-    let newMap = M.insert (hash check) check m
-    return (newMap, check)
+insertCheck :: DB -> Check -> STM ()
+insertCheck db check = do
+  modifyTVar' db $ M.insert (hash check) check
 
 newCheck :: DB -> Server PostCheckRoute
 newCheck db (NewCheckInput { .. }) =
@@ -53,12 +50,12 @@ newCheck db (NewCheckInput { .. }) =
     throwError err500 { errBody = "target must be between 0 and 100"}
   else do
     c <- liftIO $ generateCheck desc trg
-    liftIO $ insertCheck db c
+    liftIO $ atomically $ insertCheck db c
     return c
 
 getCheckFromMap throw db hash =
   let
-    succeed = \v -> liftIO $ M.lookup v <$> readMVar db
+    succeed = \v -> liftIO $ atomically $ M.lookup v <$> readTVar db
   in
     maybe throw succeed $ UUID.fromString hash
 
@@ -76,18 +73,16 @@ putCheck db hash_ = do
   case existing of
     (Just check) -> do
       solution <- liftIO $ solveCheck check
-      liftIO $ modifyMVar db $ \m -> do
-        let update _ _ = Just solution
-        let newMap = M.updateWithKey update (hash check) m
-        return (newMap, solution)
-
+      liftIO $ atomically $ modifyTVar' db $
+        M.updateWithKey (\ _ _ -> Just solution) (hash check)
+      return solution
     Nothing -> checkNotFound
 
 
 
 listCheck :: DB -> Server ListChecksRoute
 listCheck db = do
-  m <- liftIO $ readMVar db
+  m <- liftIO $ atomically $ readTVar db
   return $ fmap snd $ M.toList m
 
 
@@ -118,5 +113,5 @@ resourcePolicy =
 
 server :: IO Application
 server = do
-  db <- newMVar M.empty
+  db <- newTVarIO M.empty
   return $ ourCors $ serve userAPI $ apiServer db
