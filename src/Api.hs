94099ba68bac5c633ee99be9d566fac95cd9a815
Matteo Acerbi <matteo.acerbi@gmail.com>
2017-03-10 17:04:21 +0100

    WIP GHC Error
    ghc: panic! (the 'impossible' happened)
      (GHC version 8.0.2 for x86_64-unknown-linux):
            initTc: unsolved constraints
      WC {wc_insol = [W] hash__au2L :: t_au2K[tau:1] (CHoleCan: hash_)}
    
    Please report this as a GHC bug:  http://www.haskell.org/ghc/reportabug
    
    I guess it's this: https://ghc.haskell.org/trac/ghc/ticket/12921

--- src/Api.hs
+++ src/Api.hs
@@ -1,9 +1,11 @@
 {-# OPTIONS -Wall #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Api
     (
@@ -12,18 +14,19 @@ module Api
 
 
 import Check
+import Data.Acid
+import Data.SafeCopy
+import Data.Typeable
 import Servant.API
 import Servant
+import Control.Monad.Reader.Class
+import Control.Monad.State.Class
 import Control.Monad.IO.Class (liftIO)
-import Control.Concurrent.STM
 import Control.Monad.Trans.Except
 import qualified Data.Map.Strict as M
 import qualified Data.UUID as UUID
 import Network.Wai
 import Network.Wai.Middleware.Cors
-
-
-
 
 type PostCheckRoute = "check" :> "new"
   :> ReqBody '[JSON] NewCheckInput
@@ -39,20 +42,34 @@ type DadiAPI = PostCheckRoute
           :<|> ListChecksRoute
 
 
-type DB = TVar Collection
+data DB = DB { unDB :: Collection } deriving (Show, Typeable)
 
+-- $(deriveSafeCopy 0 'base ''DB)
 
-insertCheck :: DB -> Check -> STM ()
-insertCheck db c = do
-  modifyTVar' db $ M.insert (hash c) c
+insertCheck :: Check -> Update DB ()
+insertCheck c = modify $ (DB . M.insert (hash c) c . unDB)
 
-newCheck :: DB -> Server PostCheckRoute
+getCollection :: Query DB Collection
+getCollection = ask >>= unDB
+
+solveCheckAcid :: Check -> Int -> Update DB (Maybe Check)
+solveCheckAcid c solution = do
+  DB m <- get
+  maybeCheck <- maybe (return Nothing) (\ k -> M.lookup k m) $ UUID.fromString hash_
+  flip traverse maybeCheck $ \ c -> do
+    let c' = solveCheck c solution
+    modify $ DB . M.updateWithKey (\ _ _ -> Just c') (hash c) . unDB
+    return c'
+
+$(makeAcidic ''DB ['insertCheck, 'getCollection])
+
+newCheck :: AcidDB -> Server PostCheckRoute
 newCheck db (NewCheckInput { .. }) =
   if trg < 0 || trg > 100 then
     throwError err500 { errBody = "target must be between 0 and 100"}
   else do
     c <- liftIO $ generateCheck desc trg
-    liftIO $ atomically $ insertCheck db c
+    liftIO $ update db $ insertCheck c
     return c
 
 
@@ -60,10 +77,12 @@ newCheck db (NewCheckInput { .. }) =
 checkNotFound :: ExceptT ServantErr IO a
 checkNotFound = throwError err404 { errBody = "No Check found" }
 
-getCheck :: DB -> Server GetCheckRoute
+getCheck :: AcidDB -> Server GetCheckRoute
 getCheck db hash =
   let
-    succeed = \v -> liftIO $ atomically $ M.lookup v <$> readTVar db
+    q :: UUID.UUID -> Query DB (Maybe Check)
+    q k = ask >>= return $ M.lookup v . unDB
+    succeed k = liftIO $ query db (q k)
   in
     maybe checkNotFound succeed $ UUID.fromString hash
 
@@ -71,12 +90,7 @@ getCheck db hash =
 putCheck :: DB -> Server PutCheckRoute
 putCheck db hash_ = do
   solution <- liftIO pickSolution
-  m <- liftIO $ atomically $ do
-    maybeCheck <- maybe (return Nothing) (\ k -> M.lookup k <$> readTVar db) $ UUID.fromString hash_
-    flip traverse maybeCheck $ \ c -> do
-        let c' = solveCheck c solution
-        modifyTVar' db $ M.updateWithKey (\ _ _ -> Just c') (hash c)
-        return c'
+  m <- liftIO $ update db (solveCheckAcid hash_)
   maybe checkNotFound return m
 
 listCheck :: DB -> Server ListChecksRoute
@@ -114,5 +128,5 @@ resourcePolicy =
 
 server :: IO Application
 server = do
-  db <- newTVarIO M.empty
+  db <- openLocalState $ DB M.empty
   return $ ourCors $ serve userAPI $ apiServer db
